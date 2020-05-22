767b4fe55d0099f26540a82f4bd0ac8986d27e5a
Edenilton <edeniltonborba.adm@gmail.com>
2020-05-22 15:55:15 +0200

    initial

---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <React.Fragment>
+            <h1>
+                Find Charity
+            </h1>
+        </React.Fragment>
+    )
+}
+export default Home;
