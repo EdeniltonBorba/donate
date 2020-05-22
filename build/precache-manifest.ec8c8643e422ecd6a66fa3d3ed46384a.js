b34dc1b6a50f1905bbf98dfb56f20e7f001dee36
Edenilton <edeniltonborba.adm@gmail.com>
2020-05-22 15:35:02 +0200

    initial

---
+++ build/precache-manifest.ec8c8643e422ecd6a66fa3d3ed46384a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dfe661847391c3c6334e824c9f93c31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60cad58633c8311e8be0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad075e68631f83e25fbf",
+    "url": "/static/js/2.2f5ef2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2f5ef2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60cad58633c8311e8be0",
+    "url": "/static/js/main.e13d1641.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
