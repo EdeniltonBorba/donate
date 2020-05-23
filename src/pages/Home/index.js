176342be8effcc53a6d2dd9df8e2f0212c68cbee
Edenilton <edeniltonborba.adm@gmail.com>
2020-05-23 18:28:45 +0200

    add router

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <React.Fragment>
+            <>
+                Donate
+            </>
+        </React.Fragment>
+    )
+}
+
+export default Home;
