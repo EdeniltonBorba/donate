4eba0db2ea0ab80bb941a8fa7e12fab193d00c1d
Edenilton <edeniltonborba.adm@gmail.com>
2020-05-23 17:09:07 +0200

    add header

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <React.Fragment>
+            <h1>
+                Find Charity
+            </h1>
+        </React.Fragment>
+    )
+}
+export default Header;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Header from '../../components/Header';
 
 function Home() {
     return (
         <React.Fragment>
-            <h1>
-                Find Charity
-            </h1>
+            <div className='header'>
+                <Header />
+            </div>
         </React.Fragment>
     )
 }
