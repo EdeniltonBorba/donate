fe378ef1d514f68854dc0e9aa5857ccdd0c232f1
Edenilton <edeniltonborba.adm@gmail.com>
2020-05-24 17:01:18 +0200

    components

--- src/Page/Home/index.js
+++ src/Page/Home/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import Toolbar from '@material-ui/core/Toolbar';
-import Logo from '../../components/Logo/index.js';
 import Header from '../../components/Header/index.js';
 
 function Home() {
     return (
         <React.Fragment>
-            <Header>
-                <Toolbar>
-                    <Logo />
-                </Toolbar>
-            </Header>
+            <Header />
         </React.Fragment>
     );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { HeaderStyle } from './styles.js';
+import Logo from '../Logo/index.js';
 
 function Header() {
     return (
         <div>
             <HeaderStyle>
-                Donate
+                <Logo />
             </HeaderStyle>
         </div>
+
     );
 }
 
