d5ad6020929f9d8a3987c2a7e117448f369ddc52
Edenilton <edeniltonborba.adm@gmail.com>
2020-05-28 13:30:46 +0200

    add stylesGlobal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import Home from './Page/Home/index.js';
-import GlobalStyle from '../src/styles/Global.js';
+
+import GlobalStyle from './styles/global';
 
 export default function App() {
   return (
-    <Router>
-      <React.Fragment>
-        <GlobalStyle />
-        <div>
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </div>
-      </React.Fragment>
-    </Router>
+
+    <GlobalStyle />
+
   );
 }
