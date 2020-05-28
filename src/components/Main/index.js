038aef768a09d59bc138c35b466d7c4a8eacac62
Edenilton <edeniltonborba.adm@gmail.com>
2020-05-28 17:24:36 +0200

    add search

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import CountrySelect from '../Search/countrySelect.js';
 import OrganizationSearch from '../Search/organizationSearch.js';
-import { Container } from './styles.js';
+import { Container, Search } from './styles.js';
 
 export default function Main() {
     return (
@@ -9,9 +9,11 @@ export default function Main() {
             <Container>
                 <h1>You can change the world.</h1>
                 <h1>DONATE gives you all the tools to do it</h1>
-                <CountrySelect />
-                <CountrySelect />
-                <OrganizationSearch />
+                <Search>
+                    <CountrySelect />
+                    <CountrySelect />
+                    <OrganizationSearch />
+                </Search>
             </Container>
         </>
     );
--- src/components/Main/styles.js
+++ src/components/Main/styles.js
@@ -3,3 +3,11 @@ import styled from 'styled-components';
 export const Container = styled.div`
 color:#fff;
 `;
+
+export const Search = styled.div`
+display:flex;
+background: #ffffff;
+width: 900px;
+height:100px;
+margin: 30px;
+`;
