d3fe4c86f036fe0dbfa43575908c115730a8f5d1
Edenilton <edeniltonborba.adm@gmail.com>
2020-05-28 15:17:22 +0200

    add stylesGlobal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
+import Main from './components/Main';
 
 export default function App() {
   return (
     <>
       <Header />
+      <Main />
 
       <GlobalStyle />
     </>
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Container } from './styles';
 import CountrySelect from '../Search/countrySelect.js';
 
 export default function Main() {
     return (
-        <Container>
+        <>
+
             <h1>You can change the world.</h1>
             <h1>DONATE gives you all the tools to do it</h1>
             <CountrySelect />
-        </Container>
+
+        </>
     );
 }
