fd328144ea507a659d8045357dbecd008bec35f6
Edenilton <edeniltonborba.adm@gmail.com>
2020-05-28 16:53:00 +0200

    add search

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import CountrySelect from '../Search/countrySelect.js';
 import OrganizationSearch from '../Search/organizationSearch.js';
+import { Container } from './styles.js';
 
 export default function Main() {
     return (
         <>
-
-            <h1>You can change the world.</h1>
-            <h1>DONATE gives you all the tools to do it</h1>
-            <CountrySelect />
-            <CountrySelect />
-            <OrganizationSearch />
-
+            <Container>
+                <h1>You can change the world.</h1>
+                <h1>DONATE gives you all the tools to do it</h1>
+                <CountrySelect />
+                <CountrySelect />
+                <OrganizationSearch />
+            </Container>
         </>
     );
 }
