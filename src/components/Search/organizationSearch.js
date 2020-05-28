55f263ae09de4c5f4096480d6bbe7c384e4f5874
Edenilton <edeniltonborba.adm@gmail.com>
2020-05-28 15:57:34 +0200

    add search

--- src/components/Search/organizationSearch.js
+++ src/components/Search/organizationSearch.js
@@ -7,8 +7,8 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
     root: {
         '& .MuiTextField-root': {
-            margin: theme.spacing(1),
-            width: '25ch',
+            margin: theme.spacing(0),
+            width: 300,
         },
     },
 }));
