09eec48cf9ec72724b4329d9743527ff1614fe3d
Edenilton <edeniltonborba.adm@gmail.com>
2020-05-29 14:38:34 +0200

    search

--- src/components/Main/styles.js
+++ src/components/Main/styles.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 export const Container = styled.div`
 color:#fff;
 font-size: 30px;
+width:1100px;
+
 div {
 
 }
@@ -13,4 +15,5 @@ display:flex;
 background: #ffffff;
 height:100px;
 border-radius: 3px;
+
 `;
