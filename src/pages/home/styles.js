cce37e1aaeb2545d760bc079d77ce9a99bfa380b
Edenilton <edeniltonborba.adm@gmail.com>
2020-05-29 10:46:20 +0200

    background

--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -3,5 +3,15 @@ import img from '../../assets/image/image1.jpg';
 
 export const Container = styled.div`
 background-image: url(${img});
-background-size: contain;
+background-size: cover;
+background-position: center center;
+border: 0;
+height: 90vh;
+margin: 0;
+display: flex;
+padding: 0;
+overflow: hidden;
+position: relative;
+max-height: 1000px;
+align-items: center;
 `;
