d04b24b471d72ef50601b81628cd7434b257d321
Edenilton <edeniltonborba.adm@gmail.com>
2020-05-31 18:36:09 +0200

    add api

--- src/pages/board/index.js
+++ src/pages/board/index.js
@@ -1,7 +1,33 @@
 import React, { useState } from 'react';
+import { query } from 'express';
+import API from '../../utils/API';
 
 
 export default function Board() {
+    state = {
+        result: {},
+        search: ""
+    };
+
+    searchProject = query => {
+        API.search(query)
+            .then(res => this.setState({ result: res.data }))
+            .catch(err => console.log(err));
+    };
+
+    handleInputChange = event => {
+        const value = event.target.value;
+        const name = event.target.name;
+        this.setState({
+            [name]: value
+        });
+    };
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        this.searchProject(this.state.search);
+    };
+
     return (
         <React.Fragment>
             <h3>Result</h3>
