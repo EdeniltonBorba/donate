df916086f0ceb2eea47395f42263ad3dda8e7155
Edenilton <edeniltonborba.adm@gmail.com>
2020-05-31 18:14:15 +0200

    add api

--- src/pages/board/index.js
+++ src/pages/board/index.js
@@ -1,18 +1,7 @@
 import React, { useState } from 'react';
-import axios from 'axios';
+
 
 export default function Board() {
-    const [country, setCountry] = useState(false);
-
-    let getCountry = async () => {
-        let res = await axios.get("https://api.globalgiving.org/api/public/projectservice/countries/{iso3166CountryCode}/projects", {
-            params: {
-                appid: process.env.REACT_APP_OPEN_GLOBALGIVING_KEY,
-                lang: "pt"
-            }
-        });
-        setCountry(res.data);
-    }
     return (
         <React.Fragment>
             <h3>Result</h3>
