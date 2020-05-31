726cf8ca8f0c1e6d96bae6aabec3e6038d3da40a
Edenilton <edeniltonborba.adm@gmail.com>
2020-05-31 18:11:15 +0200

    add api

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
-const BASEURL = '';
-const APIKEY = '';
+const BASEURL = 'https://api.globalgiving.org/api/public/services/search/project/';
+const APIKEY = '&apikey=REACT_APP_OPEN_GLOBALGIVING_KEY';
 
 export default {
     search: function (query) {
